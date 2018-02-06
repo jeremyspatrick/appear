e4eaf3839c9c475f7ec2444a15f4c93fa61d96fc
Jeremy <jeremy.scott.patrick@gmail.com>
2018-02-06 17:18:43 -0500

    Update appear.js

--- dist/appear.js
+++ dist/appear.js
@@ -1,248 +1,250 @@
-/* appear.js 1.0.3 */
-appear = (function(){
-  'use strict';
-  var scrollLastPos = null, scrollTimer = 0, scroll = {};
+var tracker = function (opts) { 
 
-  function track(){
-    var newPos = window.scrollY || window.pageYOffset;  // pageYOffset for IE9
-    if ( scrollLastPos != null ){
-      scroll.velocity = newPos - scrollLastPos;
-      scroll.delta = (scroll.velocity >= 0) ? scroll.velocity : (-1 * scroll.velocity);
-      
-    }
-    scrollLastPos = newPos;
-    if(scrollTimer){
-      clearTimeout(scrollTimer);
-    }
-    scrollTimer = setTimeout(function(){
-      scrollLastPos = null;
-    }, 30);
-  }
-  addEventListener('scroll', track, false);
 
-  // determine if a given element (plus an additional "bounds" area around it) is in the viewport
-  function viewable(el, bounds){
-    var rect = el.getBoundingClientRect();
-    return (
-      (rect.top + rect.height) >= 0 &&
-      (rect.left + rect.width) >= 0 &&
-      (rect.bottom - rect.height) <= ( (window.innerHeight || document.documentElement.clientHeight) + bounds) &&
-      (rect.right - rect.width) <= ( (window.innerWidth || document.documentElement.clientWidth) + bounds)
-    );
-  }
+    ///*********************
+    /// Private variables
+    ///*********************
 
-  return function(obj){
+    var options = {};
+    var scroll = {};
+    var elements = [];
+    var reappear = [];
+    var appeared = 0;
+    var disappeared = 0;
+    var timer;
+    var deltaSet;
+    var done = false;
+    var elementsLength = 0;
+    var scrollLastPos = null;
+    var scrollTimer = null;
 
-    return (function(obj){
-      var initd = false, elements = [], elementsLength, reappear = [],
-        appeared = 0, disappeared = 0, timer, deltaSet, opts = {}, done;
 
-      // handle debouncing a function for better performance on scroll
-      function debounce(fn, delay) {
-        return function () {
-          var self = this, args = arguments;
-          clearTimeout(timer);
-          
-          timer = setTimeout(function () {
-            fn.apply(self, args);
-          }, delay);
-        };
-      }
-
-      // called on scroll and resize event, so debounce the actual function that does
-      // the heavy work of determining if an item is viewable and then "appearing" it
-      function checkAppear() {
-        if(scroll.delta < opts.delta.speed) {
-          if(!deltaSet) {
-            deltaSet = true;
-            doCheckAppear();
-            setTimeout(function(){
-              deltaSet = false;
-            }, opts.delta.timeout);
-          }
-        }
-        (debounce(function() {
-          doCheckAppear();
-        }, opts.debounce)());
-      }
-
-      function begin() {
-        // initial appear check before any scroll or resize event
-        doCheckAppear();
-
-        // add relevant listeners
-        addEventListener('scroll', checkAppear, false);
-        addEventListener('resize', checkAppear, false);
-      }
-
-      function end() {
-        elements = [];
-        if(timer) {
-          clearTimeout(timer);
-        }
-        removeListeners();
-      }
-
-      function removeListeners() {
-        
-        removeEventListener('scroll', checkAppear, false);
-        removeEventListener('resize', checkAppear, false);
-      }
-
-      function doCheckAppear() {
-        if(done) {
-          return;
-        }
-        
-        elements.forEach(function(n, i){
-          if(n && viewable(n, opts.bounds)) {
-            // only act if the element is eligible to reappear
-            if(reappear[i]) {
-              // mark this element as not eligible to appear
-              reappear[i] = false;
-              // increment the count of appeared items
-              appeared++;
-              
-              // call the appear fn
-              if(opts.appear) {
-                opts.appear(n);
-              }
-              // if not tracking reappears or disappears, need to remove node here
-              if(!opts.disappear && !opts.reappear) {
-                // stop tracking this node, which is now viewable
-                elements[i] = null;
-              }
-            }
-          } else {
-            if(reappear[i] === false) {
-              if(opts.disappear) {
-                opts.disappear(n);
-              }
-              // increment the dissappeared count
-              disappeared++;
-              
-              // if not tracking reappears, need to remove node here
-              if(!opts.reappear) {
-                // stop tracking this node, which is now viewable
-                elements[i] = null;
-              }
-            }
-            // element is out of view and eligible to be appeared again
-            reappear[i] = true;
-          }
-        });
-
-        // remove listeners if all items have (re)appeared
-        if(!opts.reappear && (!opts.appear || opts.appear && appeared === elementsLength) && (!opts.disappear || opts.disappear && disappeared === elementsLength)) {
-          // ensure done is only called once (could be called from a trailing debounce/throttle)
-          done = true;
-          removeListeners();
-          // all items have appeared, so call the done fn
-          if(opts.done){
-            opts.done();
-          }
-        }
-      }
-
-      function init() {
-        // make sure we only init once
-        if(initd) {
-          return;
-        }
-        initd = true;
-
-        // call the obj init fn
-        if(opts.init) {
-          opts.init();
-        }
-        // get the elements to work with
-        var els;
-        if(typeof opts.elements === 'function') {
-          els = opts.elements();
-        } else {
-          els = opts.elements;
-        }
-        if(els) {
-          //  put elements into an array object to work with
-          elementsLength = els.length;
-          for(var i = 0; i < elementsLength; i += 1) {
-            elements.push(els[i]);
-            reappear.push(true);
-          }
-          begin();
-        }
-      }
-
-      return function(obj) {
-        obj = obj || {};
-
-        // assign the fn to execute when a node is visible
-        opts = {
-          // a function to be run when the dom is ready (allows for any setup work)
-          init: obj.init,
-          // either an array of elements or a function that will return an htmlCollection
-          elements: obj.elements,
-          // function to call when an element is "viewable", will be passed the element to work with
-          appear: obj.appear,
-          // function to call when an element is no longer "viewable", will be passed the element to work with
-          disappear: obj.disappear,
-          // function to call when all the elements have "appeared"
-          done: obj.done,
-          // keep tracking the elements
-          reappear: obj.reappear,
-          // the extra border around an element to make it viewable outside of the true viewport
-          bounds: obj.bounds || 0,
-          // the debounce timeout
-          debounce: obj.debounce || 50,
-          // appear.js will also check for items on continuous slow scrolling
-          // you can controll how slow the scrolling should be  (deltaSpeed)
-          // and when it will check again (deltaTimeout) after it has inspected the dom/viewport;
-          delta: {
-            speed: obj.deltaSpeed || 50,
-            timeout: obj.deltaTimeout || 500
-          }
+    var init = function (opts) {
+        options = opts || {};
+        options.context = options.context || window;
+        options.bounds = options.bounds || 0;
+        options.debounce = options.debounce || 50;
+        options.delta = {
+            speed: options.deltaSpeed || 50,
+            timeout: options.deltaTimeout || 500
         };
 
-        // add an event listener to init when dom is ready
-        addEventListener('DOMContentLoaded', init, false);
+        //using window context
+        if (typeof options.context.scrollY !== 'undefined') {
+            options.context.scrollTop = options.context.scrollY;
+            options.context.offsetTop = options.context.pageYOffset;
+            options.context.clientHeight = options.context.innerHeight;
+        }
 
+
+
+
+
+        // http://stackoverflow.com/questions/9900311/how-do-i-target-only-internet-explorer-10-for-certain-situations-like-internet-e/13971998#13971998
         var isIE10 = false;
-        if (Function('/*@cc_on return document.documentMode===10@*/')()){
-          isIE10 = true;
+        if (Function('/*@cc_on return document.documentMode===10@*/')()) {
+            isIE10 = true;
         }
         var completeOrLoaded = document.readyState === 'complete' || document.readyState === 'loaded';
 
         // call init if document is ready to be worked with and we missed the event
         if (isIE10) {
-          if (completeOrLoaded) {
-            init();
-          }
+            if (completeOrLoaded) {
+                setElement();
+            }
         } else {
-          if (completeOrLoaded || document.readyState === 'interactive') {
-            init();
-          }
+            if (completeOrLoaded || document.readyState === 'interactive') {
+                setElement();
+            }
         }
 
-        return {
-          // manually fire check for visibility of tracked elements
-          trigger: function trigger(){
-            doCheckAppear();
-          },
-          // pause tracking of elements
-          pause: function pause(){
-            removeListeners();
-          },
-          // resume tracking of elements after a pause
-          resume: function resume(){
-            begin();
-          },
-          // provide a means to stop monitoring all elements
-          destroy: function destroy() {
-            end();
-          }
-        };
+        // add an event listener to init when dom is ready
+        options.context.addEventListener('DOMContentLoaded', setElement, false);
+        options.context.addEventListener('scroll', track, false);
+    }
 
-      };
-    }()(obj));
-  };
-}());
+    var setElement = function () {
+        // get the elements to work with
+        var els;
+        if (typeof options.element === 'function') {
+            els = options.element();
+            elementsLength = els.length;
+            for (var i = 0; i < elementsLength; i += 1) {
+                elements.push(els[i]);
+                reappear.push(true);
+            }
+        } else {
+            if (typeof options.element !== 'undefined'){
+                elements.push(options.element);
+                reappear.push(true);
+            }
+            
+        }
+        begin();
+
+    }
+
+    var track = function () {
+        //using window context
+        if (typeof options.context.scrollY !== 'undefined') {
+            options.context.scrollTop = options.context.scrollY;
+            options.context.offsetTop = options.context.pageYOffset;
+            options.context.clientHeight = options.context.innerHeight;
+        }
+
+        var newPos = options.context.scrollTop;  // pageYOffset for IE9
+        if (typeof scrollLastPos !== 'undefined' && scrollLastPos != null) {
+            scroll.velocity = newPos - scrollLastPos;
+            scroll.delta = (scroll.velocity >= 0) ? scroll.velocity : (-1 * scroll.velocity);
+
+        }
+        scrollLastPos = newPos;
+        if (typeof scrollTimer !== 'undefined') {
+            clearTimeout(scrollTimer);
+        }
+        scrollTimer = setTimeout(function () {
+            scrollLastPos = null;
+        }, 30);
+    }
+
+    var begin = function () {
+        // initial appear check before any scroll or resize event
+        doCheckAppear();
+
+        // add relevant listeners
+        options.context.addEventListener('scroll', checkAppear, false);
+        options.context.addEventListener('resize', checkAppear, false);
+    }
+
+    // called on scroll and resize event, so debounce the actual function that does
+    // the heavy work of determining if an item is viewable and then "appearing" it
+    var checkAppear = function () {
+        if (scroll.delta < options.delta.speed) {
+            if (!deltaSet) {
+                deltaSet = true;
+                doCheckAppear();
+                setTimeout(function () {
+                    deltaSet = false;
+                }, options.delta.timeout);
+            }
+        }
+        debounce();
+    }
+
+    // handle debouncing a function for better performance on scroll
+    var debounce = function () {
+
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            doCheckAppear();
+        }, options.debounce);
+
+    }
+
+    var doCheckAppear = function () {
+        if (done) {
+            return;
+        }
+
+        elements.forEach(function (n, i) {
+            if (n && viewable(n, options.bounds)) {
+                // only act if the element is eligible to reappear
+                if (reappear[i]) {
+                    // mark this element as not eligible to appear
+                    reappear[i] = false;
+                    // increment the count of appeared items
+                    appeared++;
+
+                    // call the appear fn
+                    if (options.appear) {
+                        options.appear(n);
+                    }
+                    // if not tracking reappears or disappears, need to remove node here
+                    if (!options.disappear && !options.reappear) {
+                        // stop tracking this node, which is now viewable
+                        elements[i] = null;
+                    }
+                }
+            } else {
+                if (reappear[i] === false) {
+                    if (options.disappear) {
+                        options.disappear(n);
+                    }
+                    // increment the dissappeared count
+                    disappeared++;
+
+                    // if not tracking reappears, need to remove node here
+                    if (!options.reappear) {
+                        // stop tracking this node, which is now viewable
+                        elements[i] = null;
+                    }
+                }
+                // element is out of view and eligible to be appeared again
+                reappear[i] = true;
+            }
+        });
+
+        // remove listeners if all items have (re)appeared
+        if (!options.reappear && (!options.appear || options.appear && appeared === elementsLength) && (!options.disappear || options.disappear && disappeared === elementsLength)) {
+            // ensure done is only called once (could be called from a trailing debounce/throttle)
+            done = true;
+            removeListeners();
+            // all items have appeared, so call the done fn
+            if (options.done) {
+                options.done();
+            }
+        }
+    }
+
+    // determine if a given element (plus an additional "bounds" area around it) is in the viewport
+    var viewable = function (el, bounds) {
+        var rect = el.getBoundingClientRect();
+
+        var elementPosition = el.offsetTop - options.context.scrollTop - options.context.offsetTop;
+        var withinXRange = true; // todo (rect.top + rect.height) >= 0 && (rect.left + rect.width) >= 0;
+        var withinYRange = (-1 * el.clientHeight) - bounds < elementPosition && elementPosition < options.context.clientHeight + bounds;
+
+
+        return withinXRange && withinYRange;
+
+    }
+
+    var end = function () {
+        elements = [];
+        if (timer) {
+            clearTimeout(timer);
+        }
+        removeListeners();
+    }
+
+    var removeListeners = function () {
+        options.context.removeEventListener('scroll', checkAppear, false);
+        options.context.removeEventListener('resize', checkAppear, false);
+    }
+
+
+    ///*********************
+    /// public api
+    ///*********************
+    // manually fire check for visibility of tracked elements
+    this.trigger = function () {
+        doCheckAppear();
+    };
+    // pause tracking of elements
+    this.pause = function () {
+        removeListeners();
+    };
+    // resume tracking of elements after a pause
+    this.resume = function () {
+        begin();
+    }
+    // provide a means to stop monitoring all elements
+    this.destroy = function () {
+        end();
+    };
+
+
+    init(opts);
+
+
+}
